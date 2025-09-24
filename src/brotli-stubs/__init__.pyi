74b6a19fe469ab0275aa1e7206b60d4856a1f2e0
Thomas Carroll <thomas.carroll@kroll.com>
2025-09-24 12:17:28 +0100

    feat: bit nicer typehinting

--- src/brotli-stubs/__init__.pyi
+++ src/brotli-stubs/__init__.pyi
@@ -1,12 +1,39 @@
-class BrotliError(Exception): ...
+from typing import Final
 
+# The library version.
+version: Final[str]
+__version__: Final[str]
+
+# The compression mode.
+MODE_GENERIC: Final[int]
+MODE_TEXT: Final[int]
+MODE_FONT: Final[int]
+
+# Raised if compression or decompression fails.
+class error(Exception): ...
+
+# The Compressor object.
+class Compressor:
+    def __init__(
+        self, mode: int = ..., quality: int = ..., lgwin: int = ..., lgblock: int = ...
+    ) -> None: ...
+    def process(self, string: bytes) -> bytes: ...
+    def finish(self) -> bytes: ...
+
+# The Decompressor object.
+class Decompressor:
+    def decompress(self, string: bytes) -> bytes: ...
+    def is_finished(self) -> bool: ...
+    def process(self, string: bytes) -> bytes: ...
+
+# Compress a byte string.
 def compress(
     string: bytes,
-    *,
     mode: int = ...,
     quality: int = ...,
     lgwin: int = ...,
     lgblock: int = ...,
-    dictionary: bytes = ...,
 ) -> bytes: ...
-def decompress(string: bytes, *, dictionary: bytes = ...) -> bytes: ...
+
+# Decompress a compressed byte string.
+def decompress(string: bytes) -> bytes: ...
